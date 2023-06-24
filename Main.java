f271c43323d3191a4e34ffd625a8ac6ab42b4d80
Biruk DAmte <birukdamte013@gmail.com>
2023-06-24 10:45:45 +0300

    By Biruk Damte

--- Main.java
+++ Main.java
@@ -5,6 +5,12 @@ public class Main {
         System.out.println(name);
         System.out.println(id_no);
     }
+    void Biruk_Damte() {
+        String name = "Biruk Damte";
+        int id_no = 0344;
+        System.out.println(name);
+        System.out.println(id_no);
+    }
 
     public static void main(String args[]) {
         System.out.println("this is our reopsitory");
