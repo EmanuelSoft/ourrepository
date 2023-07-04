889a39da33f15470a8c69f24aa5ed5ac011aa42c
sasma12 <selamd087@gmail.com>
2023-07-04 09:28:06 +0300

    by selam

--- Main.java
+++ Main.java
@@ -5,75 +5,90 @@ public class Main {
         System.out.println(name);
         System.out.println(id_no);
     }
+
     void Biruk_Damte() {
         String name = "Biruk Damte";
         int id_no = 0344;
         System.out.println(name);
         System.out.println(id_no);
     }
+
     void Asaye_Chala() {
         String name = "Asaye Chala";
         int id_no = 1579;
         System.out.println(name);
         System.out.println(id_no);
     }
-     void Bizuayhu_s() {
+
+    void Bizuayhu_s() {
         String name = "Bizuayhu Simachew";
         int id_no = 0310;
         System.out.println(name);
         System.out.println(id_no);
     }
+
     void Ermias_tesfsh() {
         String name = "Ermias Tesfaye";
         int id_no = 0214;
         System.out.println(name);
         System.out.println(id_no);
     }
+
     void Natnael_Yonas() {
         String name = "Natnael Yonas";
         int id_no = 1307331;
         System.out.println(name);
         System.out.println(id_no);
     }
+
     void Biruk_Kasahun() {
         String name = "Biruk kasahun";
         int id_no = 1253;
         System.out.println(name);
         System.out.println(id_no);
     }
+
     void selamawit_Tolera() {
         String name = "selamawit Tolera";
         int id_no = 1056;
         System.out.println(name);
         System.out.println(id_no);
     }
+
     void natnael_geleta() {
         String name = "natnael geleta";
         int id_no = 0754;
         System.out.println(name);
         System.out.println(id_no);
     }
-     void kaleab_tenkir() {
+
+    void kaleab_tenkir() {
         String name = "kaleab tenkir";
         int id_no = 0668;
         System.out.println(name);
         System.out.println(id_no);
     }
 
-
+    void selamawit_Tollera() {
+        String name = "selamawit Tollera";
+        int id_no = 1056;
+        System.out.println(name);
+        System.out.println(id_no);
+    }
 
     public static void main(String args[]) {
         System.out.println("this is our reopsitory");
-       Main info=new Main();
-       info.amanuel_Bekele();
-       info.Biruk_Damte();
-       info.Asaye_Chala();
-       info.Bizuayhu_s();
-       info.Ermias_tesfsh();
-       info.Natnael_Yonas();
-       info.Biruk_Kasahun();
-       info.selamawit_Tolera();
-       info.natnael_geleta();
-       info.kaleab_tenkir();
+        Main info = new Main();
+        info.amanuel_Bekele();
+        info.Biruk_Damte();
+        info.Asaye_Chala();
+        info.Bizuayhu_s();
+        info.Ermias_tesfsh();
+        info.Natnael_Yonas();
+        info.Biruk_Kasahun();
+        info.selamawit_Tolera();
+        info.natnael_geleta();
+        info.kaleab_tenkir();
+        info.selamawit_Tollera();
     }
 }
